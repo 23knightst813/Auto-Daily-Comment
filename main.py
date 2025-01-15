ad5024ed8031356ef1d047100a9a7bdff533339a
Theo <23knightst813@collyers.ac.uk>
2025-01-15 21:25:14 +0000

    Add environment variable support for ChromeDriver and Brave Browser

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from dotenv import load_dotenv
+import os
+
+# Load the environment variables
+load_dotenv()
 
 # Set the path to the chromedriver executable
-chrome_driver_path = "C:/Users/username/Downloads/chromedriver_win32/chromedriver.exe"
+chrome_driver_path = os.getenv("DRIVER_PATH")
+
+# Set the path to the Brave Browser executable
+options = Options()
+options.binary_location = os.getenv("BROWSER_PATH")
