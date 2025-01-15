4974f27f0cc7271d329cb5b0d3b2b51c62e83c74
Theo <23knightst813@collyers.ac.uk>
2025-01-15 21:20:51 +0000

    Add initial project setup with environment configuration and dependencies

---
+++ main.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+# Set the path to the chromedriver executable
+chrome_driver_path = "C:/Users/username/Downloads/chromedriver_win32/chromedriver.exe"
