c221d6fcf49512ce50eee40735e6e0d92fe32e2a
Theo <23knightst813@collyers.ac.uk>
2025-01-15 21:53:52 +0000

    Refactor environment variable handling and update comment posting logic

--- main.py
+++ main.py
@@ -8,30 +8,31 @@ from dotenv import load_dotenv
 import os
 
 
-# Load the environment variables
 load_dotenv()
 
-# Increment the day
 new_day = int(os.getenv('DAY')) + 1
 os.environ['DAY'] = str(new_day)
 
-#Build The Comment
 comment = f"Day {new_day} of 100 of {os.getenv('CHALLENGE_NAME')}"
 
-# Set the path to the chromedriver executable
-driver_path = os.getenv("DRIVER_PATH")
 
-# Set the path to the Brave Browser executable
-options = Options()
-options.binary_location = os.getenv("BROWSER_PATH")
+driver_path = os.path.join(os.path.dirname(__file__), os.getenv('DRIVER_PATH'))
 
-#initialize the browser
+brave_path = r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
+
+options = webdriver.ChromeOptions()
+options.binary_location = brave_path
+
 driver = webdriver.Chrome(service=Service(driver_path), options=options)
 
-# Open the Instagram page
 driver.get('https://www.instagram.com/openminded.podcast/reels')
 
-# Xpath to the reels
+# Click Accept Cookies
+accept_cookies_xpath = "//button[contains(text(), 'Allow all cookies')]"
+WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, accept_cookies_xpath))
+).click()
+
 reels_xpath = "//div[contains(@class, 'v1Nh3')]/a"
 
 WebDriverWait(driver, 10).until(
@@ -45,23 +46,16 @@ reels = driver.find_elements(By.XPATH, reels_xpath)
 reels[0].click()
 
 
-# Xpath to the comment textarea
 comment_xpath = "//textarea[@aria-label='Add a comment…']"
 
-# Wait for the comment textarea to be present
 WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.XPATH, comment_xpath))
 )
 
-# Find the comment textarea
 comment_box = driver.find_element(By.XPATH, comment_xpath)
 
-# Type the comment
 comment_box.send_keys(str(comment))
 
-# Submit the comment
 comment_box.send_keys("\n")
 
-# Close the browser
 driver.quit()
-
